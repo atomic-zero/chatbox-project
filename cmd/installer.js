9a85bde6145c62380a81990da5ec5a623532a163
atomic-zero <lkpanio25@gmail.com>
2024-06-08 10:41:34 +0800

    update installer.js

--- cmd/installer.js
+++ cmd/installer.js
@@ -7,86 +7,44 @@ module.exports = {
   type: "installation",
   credits: "Kenneth Panio",
   version: "1.1.0",
-exec({ chat, event, args }) {
-  const fs = require('fs');
-  const path = require('path');
-  const { transpileModule } = require('typescript');
+  async exec({ chat, event, args }) {
+    const fs = require('fs');
+    const path = require('path');
+    const { transpileModule } = require('typescript');
 
-  const uninstallKeywords = ["uninstall", "delete", "remove", "rm", "del"];
+    const uninstallKeywords = ["uninstall", "delete", "remove", "rm", "del"];
 
-  const listCommands = () => {
-    const files = fs.readdirSync(__dirname);
-    const commandFiles = files.filter(file => file.endsWith('.js') || file.endsWith('.ts'));
-    const commands = commandFiles.map((file, index) => `${index + 1}. ${file.replace(/\.[^/.]+$/, '')}`);
-    return commands.length ? commands.join('\n') : 'No commands installed.';
-  };
+    const listCommands = () => {
+      const files = fs.readdirSync(__dirname);
+      const commandFiles = files.filter(file => file.endsWith('.js') || file.endsWith('.ts'));
+      const commands = commandFiles.map((file, index) => `${index + 1}. ${file.replace(/\.[^/.]+$/, '')}`);
+      return commands.length ? commands.join('\n') : 'No commands installed.';
+    };
 
-  if (args[0] === 'list') {
-    return chat.reply(listCommands());
-  }
-
-  if (uninstallKeywords?.includes(args[0])) {
-    if (args.length !== 2) {
-      return chat.reply('Please provide the filename of the command to uninstall.');
+    if (args[0] === 'list') {
+      return chat.reply(listCommands());
     }
-    const fileName = args[1];
-    const filePath = path.join(__dirname, fileName);
 
-    fs.unlink(filePath, function(err) {
-      if (err) {
-        return chat.reply(`An error occurred while uninstalling the command: ${err.message}`);
-      } else {
-        return chat.reply(`Command ${fileName} successfully uninstalled.`);
+    if (uninstallKeywords.includes(args[0])) {
+      if (args.length !== 2) {
+        return chat.reply('Please provide the filename of the command to uninstall.');
       }
-    });
-  } else {
-    if (event.type === "message_reply") {
-      const snippet = args.slice(1).join(' ');
-      const text = event.messageReply.body || snippet;
-
-      if (!text.trim()) {
-        return chat.reply('No code provided for installation.');
-      }
-
-      const fileName = args[0];
-      const fileExtension = fileName.split('.').pop()?.toLowerCase();
-
-      if (fileExtension !== 'js' && fileExtension !== 'ts') {
-        return chat.reply('Invalid file extension. Please provide a .js or .ts file.');
-      }
-
-      if (fileExtension === 'js') {
-        try {
-          eval(text); 
-        } catch (error) {
-          return chat.reply(`Syntax error detected in the provided JavaScript code: ${error.message}`);
-        }
-      }
-
-      if (fileExtension === 'ts') {
-        try {
-          transpileModule(text, {}); 
-        } catch (error) {
-          return chat.reply(`Syntax error detected in the provided TypeScript code: ${error.message}`);
-        }
-      }
-
+      const fileName = args[1];
       const filePath = path.join(__dirname, fileName);
-      
-      fs.writeFile(filePath, text, "utf-8", function (err) {
+
+      fs.unlink(filePath, function(err) {
         if (err) {
-          return chat.reply(`An error occurred while installing the command: ${err.message}`);
+          return chat.reply(`An error occurred while uninstalling the command: ${err.message}`);
         } else {
-          return await chat.reply(`Command ${fileName} successfully installed.`);
-          await process.exit(1);
+          return chat.reply(`Command ${fileName} successfully uninstalled.`);
         }
       });
     } else {
-      if (args.length < 2) {
-        return chat.reply('Please reply to a message containing the command code or provide the code directly in the format install [filename.js or ts]. or use cmd uninstall [filename.js or ts]');
-      } else {
+      if (event.type === "message_reply") {
         const snippet = args.slice(1).join(' ');
-        if (!snippet.trim()) {
+        const text = event.messageReply.body || snippet;
+
+        if (!text.trim()) {
           return chat.reply('No code provided for installation.');
         }
 
@@ -99,7 +57,7 @@ exec({ chat, event, args }) {
 
         if (fileExtension === 'js') {
           try {
-            eval(snippet); 
+            eval(text); 
           } catch (error) {
             return chat.reply(`Syntax error detected in the provided JavaScript code: ${error.message}`);
           }
@@ -107,7 +65,7 @@ exec({ chat, event, args }) {
 
         if (fileExtension === 'ts') {
           try {
-            transpileModule(snippet, {});
+            transpileModule(text, {}); 
           } catch (error) {
             return chat.reply(`Syntax error detected in the provided TypeScript code: ${error.message}`);
           }
@@ -115,16 +73,58 @@ exec({ chat, event, args }) {
 
         const filePath = path.join(__dirname, fileName);
         
-        fs.writeFile(filePath, snippet, "utf-8", function (err) {
+        fs.writeFile(filePath, text, "utf-8", async function (err) {
           if (err) {
             return chat.reply(`An error occurred while installing the command: ${err.message}`);
           } else {
-            return await chat.reply(`Command ${fileName} successfully installed.`);
-            await process.exit(1);
+            await chat.reply(`Command ${fileName} successfully installed.`);
+            process.exit(1);
           }
         });
+      } else {
+        if (args.length < 2) {
+          return chat.reply('Please reply to a message containing the command code or provide the code directly in the format install [filename.js or ts]. or use cmd uninstall [filename.js or ts]');
+        } else {
+          const snippet = args.slice(1).join(' ');
+          if (!snippet.trim()) {
+            return chat.reply('No code provided for installation.');
+          }
+
+          const fileName = args[0];
+          const fileExtension = fileName.split('.').pop()?.toLowerCase();
+
+          if (fileExtension !== 'js' && fileExtension !== 'ts') {
+            return chat.reply('Invalid file extension. Please provide a .js or .ts file.');
+          }
+
+          if (fileExtension === 'js') {
+            try {
+              eval(snippet); 
+            } catch (error) {
+              return chat.reply(`Syntax error detected in the provided JavaScript code: ${error.message}`);
+            }
+          }
+
+          if (fileExtension === 'ts') {
+            try {
+              transpileModule(snippet, {});
+            } catch (error) {
+              return chat.reply(`Syntax error detected in the provided TypeScript code: ${error.message}`);
+            }
+          }
+
+          const filePath = path.join(__dirname, fileName);
+          
+          fs.writeFile(filePath, snippet, "utf-8", async function (err) {
+            if (err) {
+              return chat.reply(`An error occurred while installing the command: ${err.message}`);
+            } else {
+              await chat.reply(`Command ${fileName} successfully installed.`);
+              process.exit(1);
+            }
+          });
+        }
       }
     }
   }
-}
 };

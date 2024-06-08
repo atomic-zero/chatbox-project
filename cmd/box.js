2a41905328e71aa69e168a51e8012131a11a6652
atomic-zero <lkpanio25@gmail.com>
2024-06-08 09:25:20 +0800

    update box.js

---
+++ cmd/box.js
@@ -0,0 +1,69 @@
+module.exports = {
+  name: "box",
+  aliases: ["bb", "blackbox", "blackbox-ai"],
+  version: "1.0.0",
+  isPrefix: false,
+  credits: "Kenneth Panio",
+  type: "artificial-intelligence",
+  info: "Interact with Blackbox AI. Use 'box switch' to toggle web search mode.",
+  usage: "[prompt]",
+  guide: "blackbox How does nuclear fusion work?",
+  cd: 6,
+  exec: async ({ chat, args, event, fonts }) => {
+    const mono = txt => fonts.monospace(txt);
+    const axios = require("axios");
+    const conversationHistories = {};
+    let webSearchMode = false;
+    const { threadID, senderID } = event;
+    const query = args.join(" ");
+
+    if (['clear', 'reset', 'forgot', 'forget']?.includes(query?.toLowerCase())) {
+      conversationHistories[senderID] = [];
+      chat.reply(mono("Conversation history cleared."));
+      return;
+    }
+
+    if (query === 'toggle') {
+      webSearchMode = !webSearchMode;
+      chat.reply(mono(`Web search mode has been ${webSearchMode ? 'enabled' : 'disabled'}.`));
+      return;
+    }
+
+    if (!query) {
+      chat.reply(mono("Please provide a question!"));
+      return;
+    }
+
+    const answering = await chat.reply(mono("🕐 | Answering..."));
+
+    conversationHistories[senderID] = conversationHistories[senderID] || [];
+    conversationHistories[senderID].push({ content: query, role: senderID });
+
+    try {
+      const response = await axios.post("https://www.blackbox.ai/api/chat", {
+        messages: conversationHistories[senderID],
+        id: senderID,
+        previewToken: null,
+        userId: senderID,
+        codeModelMode: true,
+        agentMode: [],
+        trendingAgentMode: [],
+        isMicMode: false,
+        isChromeExt: false,
+        githubToken: null,
+        webSearchMode,
+        maxTokens: '10240'
+      });
+
+      const answer = response.data.replace(/\$@\$(.*?)\$@\$/g, '') || mono("I don't understand could you please rephrase that!");
+
+      conversationHistories[senderID].push({ content: answer, role: "assistant" });
+
+      const line = "\n" + '━'.repeat(18) + "\n";
+      answering.edit(mono("◼️ BLACKBOX AI") + line + answer + line + mono(`◉ USE "CLEAR" TO RESET CONVERSATION.\n◉ USE "TOGGLE" TO SWITCH WEBSEARCH`));
+    } catch (error) {
+      chat.error("Error processing request:" + error.message);
+      answering.edit(mono("No response from Blackbox AI. Please try again later: " + error.message));
+    }
+  }
+}
